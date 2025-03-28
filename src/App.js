29510807a007a3dd59c84257b754f80e61f1c923
miloscermak <130353544+miloscermak@users.noreply.github.com>
2025-03-28 21:01:52 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AITypologyQuiz from './components/AITypologyQuiz';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>AI Typology Quiz</h1>
+      <AITypologyQuiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AITypologyQuiz.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AITypologyQuiz() {
+  return <p>Dotazník se úspěšně načetl.</p>;
+}
